fb22a6a7a9ea250959fed59128f4e3a066551b5b
dert1129 <nathancreger1111@gmail.com>
2024-12-22 21:52:16 -0500

    change background color

--- app/home.tsx
+++ app/home.tsx
@@ -226,7 +226,7 @@ const styles = StyleSheet.create({
     },
     container: {
     flex: 1,
-    backgroundColor: "#f5f5f5"
+    backgroundColor: "#f5f5f5",
     }, 
     bookmark: {
     display: 'flex',
